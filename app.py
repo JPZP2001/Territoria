c752ca2ad6a2194b54a182de0de9443f4897897b
JP Zubieta <juan.zubieta@udem.edu>
2023-04-24 16:22:19 -0500

    Delted

--- app.py
+++ app.py
@@ -30,12 +30,6 @@ external_stylesheets = [{'href': 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/d
      'rel': 'stylesheet', 'integrity': 'sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi',
      'crossorigin': 'anonymous'}]
 
-# Initialize app
-app = Dash(__name__,
-           use_pages=True,
-           external_scripts = external_scripts,
-           external_stylesheets=external_stylesheets
-           )
 
 app.index_string = """<!DOCTYPE html>
 <html>
